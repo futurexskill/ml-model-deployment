bfc737115bf2e15a149437c293122572b4d7a030
futureXskill <54407776+futurexskill@users.noreply.github.com>
2020-10-19 14:51:26 -0700

    Add files via upload

--- ml_rest_client.py
+++ ml_rest_client.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-url = 'http://127.0.0.1:8005/model'
+url = 'http://34.68.168.133:8005/model'
 
 request_data = json.dumps({'age':40,'salary':20000})
 response = requests.post(url,request_data)
