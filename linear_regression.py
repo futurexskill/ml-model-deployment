b55735587ab8e76525d0dd7b859591ba4907c3bf
futureXskill <54407776+futurexskill@users.noreply.github.com>
2020-10-24 23:08:29 -0700

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -1,53 +1,57 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-"""## Read the dataset"""
-
-dataset = pd.read_csv('studentmarks.csv')
+dataset = pd.read_csv('houseprice.csv')
 
 dataset.describe()
-
 
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
+#plt.xlabel('Distance to City center')
+#plt.ylabel('House price USD')
+#plt.title('House price analysis')
+#plt.scatter(X, y)
+
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2,random_state=0)
-
-"""## Build a Linear Regression model"""
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .2,random_state=48)
 
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-
 rsquared = regressor.score(X_test, y_test)
 rsquared
 
-### Y = a + b X
-#Get the constant or intercept  value of the line
-
 intercept = regressor.intercept_
 intercept
-
-### Get the slope or coefficient of the line##
 
 coefficient = regressor.coef_
 coefficient
 
 ### Y = a + b X
-### Mark = 0.531  + 9.93 Hours
-## Predict for the test set
+### Price = 610710.0319872361 - 72635.89282856 Distance
+
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_test, y_test, color = 'blue')
+plt.plot(X_test, y_pred, color = 'green')
+plt.title('House price analysis')
+plt.xlabel('Distance to City center')
+plt.ylabel('House price USD')
+plt.show()
 
 
-####Predict using the Regressor Model
-
-
-y_new_pred_1 = regressor.predict([[3]])
+y_new_pred_1 = regressor.predict([[2.5]])
 print(y_new_pred_1)
 
-####Predict using the formula
 
-y_new_pred_2 = coefficient * 3 + intercept
+y_new_pred_2 = coefficient * 2.5 + intercept
 print(y_new_pred_1)
+
+
+
+
